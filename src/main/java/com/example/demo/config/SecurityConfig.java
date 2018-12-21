c4c0d8eff302795245426c1532708ec776131664
kverchi <kverchi24@gmail.com>
2018-12-21 14:33:07 +0100

    Added Spring security configuration file with in-memory user

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		auth
+		.inMemoryAuthentication()
+		.withUser("buzz")
+		.password(encoder.encode("infinity"))
+		.authorities("ROLE_USER");
+	}
+}
