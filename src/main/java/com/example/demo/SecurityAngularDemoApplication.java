b20c841e6d2a9a593553903c341bceec2e34e177
kverchi <kverchi24@gmail.com>
2018-12-21 14:03:50 +0100

    Added Spring security project. Added basic login with in-memory user store.

---
+++ src/main/java/com/example/demo/SecurityAngularDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAngularDemoApplication.class, args);
+	}
+
+}
+
